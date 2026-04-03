6ad9435e8f677cf6b6d17066fc2812ade2911c7d
akashkumars883 <ak706908@gmail.com>
2026-04-04 01:20:10 +0530

    🚀 Added Google Analytics standard integration

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import { Outfit, Vollkorn } from "next/font/google";
 import "./globals.css";
+import Script from "next/script";
 
 const outfit = Outfit({
   subsets: ["latin"],
@@ -55,6 +56,22 @@ export default function RootLayout({ children }) {
       <body
         className={`${outfit.variable} ${vollkorn.variable} antialiased`}
       >
+        {process.env.NEXT_PUBLIC_GA_ID && (
+          <>
+            <Script
+              src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}
+              strategy="afterInteractive"
+            />
+            <Script id="google-analytics" strategy="afterInteractive">
+              {`
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}');
+              `}
+            </Script>
+          </>
+        )}
         {children}
       </body>
     </html>
