4c818237c71cec3c33708baf8c8d09e75cb0e352
akashkumars883 <ak706908@gmail.com>
2026-05-16 12:08:19 +0530

    design: remove blog page loader

--- app/(public)/blogs/loading.js
+++
@@ -1,9 +0,0 @@
-import LoadingG from '@/components/LoadingG';
-
-export default function Loading() {
-  return (
-    <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-20">
-      <LoadingG />
-    </div>
-  );
-}
