264e54962aadbe8b84c171b5c973d70ab1d4ee97
akashkumars883 <ak706908@gmail.com>
2026-06-26 00:41:43 +0530

    Add Google Ads tag to layout

--- app/layout.js
+++ app/layout.js
@@ -90,6 +90,18 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en" className={`${outfit.variable}`}>
       <head>
+        {/* Google tag (gtag.js) */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=AW-18228295735"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'AW-18228295735');
+            `
+          }}
+        />
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{
