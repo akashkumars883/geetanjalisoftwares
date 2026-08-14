2a45905e4260e00505b48573462babb203133bb2
akashkumars883 <ak706908@gmail.com>
2026-08-14 16:58:12 +0530

    style: remove custom dot cursor and restore standard browser pointer

--- components/LayoutWrapper.jsx
+++ components/LayoutWrapper.jsx
@@ -4,7 +4,6 @@ import { usePathname } from "next/navigation";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 import SmoothScrollProvider from "@/components/SmoothScrollProvider";
-import CustomCursor from "@/components/CustomCursor";
 
 export default function LayoutWrapper({ children }) {
   const pathname = usePathname();
@@ -16,7 +15,6 @@ export default function LayoutWrapper({ children }) {
 
   return (
     <SmoothScrollProvider>
-      <CustomCursor />
       <Navbar />
       <div className="flex-1 flex flex-col">{children}</div>
       <Footer />
